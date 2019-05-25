fb93cc6c30d0bb17c8e256da37b75e9c4ff460ea
Francis Hwang <sera@fhwang.net>
2019-05-25 13:53:30 -0400

    Initial commit.

---
+++ lib/sync_machine_mongoid.rb
@@ -0,0 +1,6 @@
+require "sync_machine_mongoid/version"
+
+module SyncMachineMongoid
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sync_machine_mongoid/version.rb
@@ -0,0 +1,3 @@
+module SyncMachineMongoid
+  VERSION = "0.1.0"
+end
