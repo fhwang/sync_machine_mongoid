114e80b7d59c2ed533500c2b32ac6bab933879eb
Francis Hwang <sera@fhwang.net>
2019-09-07 11:34:48 -0400

    Initial functionality and specs migrated from SyncMachine. (#1)

---
+++ lib/sync_machine/mongoid/adapter.rb
@@ -0,0 +1,14 @@
+module SyncMachine
+  module Mongoid
+    # Adapt generic SyncMachine functionality to Mongoid.
+    module Adapter
+      def self.change_listener_changed_keys(record)
+        record.changes.keys
+      end
+
+      def self.record_id_for_job(record_id)
+        record_id.to_s
+      end
+    end
+  end
+end
---
+++ spec/support/models.rb
@@ -0,0 +1,29 @@
+class Customer
+  include Mongoid::Document
+
+  field :name, type: String
+
+  has_many :orders
+end
+
+class Order
+  include Mongoid::Document
+
+  field :next_payload, type: Hash, default: {"foo" => "bar"}
+  field :publishable, type: Boolean, default: true
+
+  belongs_to :customer
+end
+
+module TestSync
+  class Payload
+    include ::Mongoid::Document
+
+    field :body, type: Hash
+    field :generated_at, type: Time
+    field :subject_id, type: String
+
+    validates :generated_at, presence: true
+    validates :subject_id, presence: true, uniqueness: true
+  end
+end
